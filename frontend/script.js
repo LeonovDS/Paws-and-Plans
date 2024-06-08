91c20f7240c2561c85ebc543c11c555b5d9741a8
Antimonyzz <144496509+Antimonyzz@users.noreply.github.com>
2024-06-08 17:47:28 +0300

    firsttry

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+const words = document.querySelectorAll('.word');
+
+words.forEach(word => {
+  word.addEventListener('click', () => {
+    const tooltip = word.querySelector('.tooltip');
+    tooltip.style.display = 'block';
+  });
+});
