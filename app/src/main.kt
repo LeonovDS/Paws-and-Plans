25563a9c1743cecb329150afa2edfe89da1232bd
Daniil Leonov <yshmgrt@gmail.com>
2024-06-08 15:06:23 +0300

    Init backend project

--- app/src/main.kt
+++ app/src/main.kt
@@ -1,3 +1,15 @@
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.routing.ResourceLoader
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.routing.static
+import org.http4k.server.asServer
+import org.http4k.server.Jetty
+
 fun main() {
-    println("Hello, World!")
+    routes(
+        "/api" bind { _ -> Response(Status.OK) },
+        "/" bind static(ResourceLoader.Classpath("/static"))
+    ).asServer(config = Jetty(8080)).start()
 }
