17e7eda346bca4a2044863a71f514d413f3e00d6
Daniil Leonov <yshmgrt@gmail.com>
2024-06-15 03:02:00 +0300

    Typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker login --username oauth --passworf $OAUTH_TOKEN cr.yandex
+docker login --username oauth --password $OAUTH_TOKEN cr.yandex
 docker build . -t cr.yandex/$REGISTRY_ID/pnp
 docker push cr.yandex/$REGISTRY_ID/pnp
